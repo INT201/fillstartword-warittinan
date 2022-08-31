f0c2691eefa740380f534d82177cf80fac4fc002
warittinan <warittinan.kh@mail.kmutt.ac.th>
2022-08-31 11:57:03 +0700

    finish

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,10 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+    if (word==null && word== undefined){
+        return undefined
+    } else {
+        return startWord+word
+    }
 }
 module.exports = fillStartWord
